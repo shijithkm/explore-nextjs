3dce4d66e4e09bddb4c8312320c1e613b4379e2a
kandirithy.m@pg.com <developer.shijith@gmail.com>
2022-09-14 15:31:36 +0530

    Search Results Style Added

--- components/search/SearchResults.js
+++ components/search/SearchResults.js
@@ -13,102 +13,90 @@ export default function SearchResults({ data }) {
       <div className="w-full mb-5 mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
         <div className="flex flex-col justify-center h-full ">
           <div className="p-3">
-            <div className="overflow-x-auto">
-              <table className="table-auto w-full">
-                <tbody className="text-sm divide-y divide-gray-100">
-                  {data.map((professional) => (
-                    <tr key={professional?.id} className="bg-zinc-100">
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                            <Image
-                              className="rounded-full bg-zinc-400"
-                              src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
-                              width="40"
-                              height="40"
-                              alt="Alex Shatov"
-                            />
-                          </div>
-                          <div>
-                            <h3 className="font-bold">{professional?.name}</h3>
-                            <h5 className="text-zinc-400">
-                              Builing Contractor
-                            </h5>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <Image
-                            alt="Location Icon"
-                            width="40px"
-                            height="40px"
-                            src={Location}
-                          />
-                          <h3 className="text-left inline-block font-bold">
-                            {professional?.email}
-                          </h3>
-                        </div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <Image
-                            alt="Location Icon"
-                            width="40px"
-                            height="40px"
-                            src={Location}
-                          />
-                          <h3 className="text-left inline-block font-bold">
-                            5 Years
-                          </h3>
-                        </div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <Rating
-                            initialRating={3}
-                            readonly
-                            emptySymbol={
-                              <Image
-                                src={EmptyStar}
-                                className="icon"
-                                width="25px"
-                                height="25px"
-                              />
-                            }
-                            placeholderSymbol={
-                              <Image
-                                src={FullStar}
-                                className="icon"
-                                width="25px"
-                                height="25px"
-                              />
-                            }
-                            fullSymbol={
-                              <Image
-                                src={FullStar}
-                                className="icon"
-                                width="25px"
-                                height="25px"
-                              />
-                            }
-                          />
-                          <h3 className="text-left inline-block text-zinc-400 font-medium">
-                            (72)
-                          </h3>
-                        </div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <h3 className="text-left inline-block font-bold">
-                            1500/sqft
-                          </h3>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+            <div className="overflow">
+              {data.map((professional) => (
+                <div key={data.id} className="flex flex-row bg-zinc-100 m-4">
+                  <div className="basis-1/4 flex justify-start items-center p-2">
+                    <div className="w-10 h-10 mr-2 sm:mr-3">
+                      <Image
+                        className="rounded-full bg-zinc-400"
+                        src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
+                        width="40"
+                        height="40"
+                        alt="Alex Shatov"
+                      />
+                    </div>
+                    <div>
+                      <h3 className="font-bold">{professional?.name}</h3>
+                      <h5 className="text-zinc-400">Builing Contractor</h5>
+                    </div>
+                  </div>
+
+                  <div className="basis-1/4 flex justify-start items-center p-2">
+                    <Image
+                      alt="Location Icon"
+                      width="40px"
+                      height="40px"
+                      src={Location}
+                    />
+                    <h3 className="text-left inline-block font-bold">
+                      {professional?.email}
+                    </h3>
+                  </div>
+
+                  <div className="basis-1/4 flex justify-start items-center p-2">
+                    <Image
+                      alt="Location Icon"
+                      width="40px"
+                      height="40px"
+                      src={Location}
+                    />
+                    <h3 className="text-left inline-block font-bold">
+                      5 Years
+                    </h3>
+                  </div>
+
+                  <div className="basis-1/4 flex justify-start items-center p-2">
+                    <Rating
+                      initialRating={3}
+                      readonly
+                      emptySymbol={
+                        <Image
+                          src={EmptyStar}
+                          className="icon"
+                          width="25px"
+                          height="25px"
+                        />
+                      }
+                      placeholderSymbol={
+                        <Image
+                          src={FullStar}
+                          className="icon"
+                          width="25px"
+                          height="25px"
+                        />
+                      }
+                      fullSymbol={
+                        <Image
+                          src={FullStar}
+                          className="icon"
+                          width="25px"
+                          height="25px"
+                        />
+                      }
+                    />
+                    <h3 className="text-left inline-block text-zinc-400 font-medium px-2">
+                      (72)
+                    </h3>
+                  </div>
+
+                  <div className="basis-1/4 flex justify-start items-center p-2">
+                    <h3 className="text-left inline-block font-bold">
+                      1500/sqft
+                    </h3>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
